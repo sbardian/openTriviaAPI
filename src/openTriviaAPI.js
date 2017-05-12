36b5e2299fa03dae6b157afc7537d1ee9034e798
Brian <sbardian@gmail.com>
2017-05-12 12:44:34 -0600

    Add base app structure.

---
+++ src/openTriviaAPI.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sbardian on 5/12/17.
+ */
+
+import {axios} from 'axios';
+import {
+  OK,
+  NO_RESULTS,
+  INVALID_PARAMETER,
+  TOKEN_NOT_FOUND,
+  TOKEN_EMPTY,
+} from 'responses';
+
+const openTriviaAPI = {
+
+};
+
+export default openTriviaAPI;
