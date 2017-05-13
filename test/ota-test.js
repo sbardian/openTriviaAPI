5f822660cfa8cb98b5fd60db21d4da1fba7de952
Brian <sbardian@gmail.com>
2017-05-12 21:56:19 -0600

    Add tests for token functions.

--- test/ota-test.js
+++ test/ota-test.js
@@ -7,15 +7,33 @@ import { expect } from 'chai';
 
 describe('Test openTriviaAPI calls', () => {
   let options = {};
+  let token;
   before(() => {
     options = {
       amount: 1,
     };
   });
-  it('Should return object', (done) => {
+  it('Should return respone code 0 for success...', (done) => {
     openTriviaAPI.getQuestions(options)
         .then((data) => {
           expect(data.response_code).to.equal(0);
+          done();
+        });
+  });
+  it('Should return token...', (done) => {
+    openTriviaAPI.getToken()
+        .then((data) => {
+          token = data.token;
+          expect(data.response_code).to.equal(0);
+          expect(data.token).to.be.a('string');
+          done();
+        });
+  });
+  it('Should reset token and response_code 0...', (done) => {
+    openTriviaAPI.resetToken(token)
+        .then((data) => {
+          expect(data.response_code).to.equal(0);
+          expect(data.token).to.be.a('string');
           done();
         });
   });
